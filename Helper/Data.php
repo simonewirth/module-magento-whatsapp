fca96d2075e8ab56ff0be84e0a63538908dc9ea4
Simone Wirth <anschausimone@gmail.com>
2024-01-22 15:42:31 -0300

    Adicionando Helper/Data

---
+++ Helper/Data.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Swirth\Whatsapp\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+
+class Data extends AbstractHelper
+
+{
+    function __construct(Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    function isModuleEnabled(): bool
+    {
+        return $this->scopeConfig->isSetFlag("whatsapp/general/enabled");
+    }
+
+    function getPhoneNumber()
+    {
+        return $this->scopeConfig->getValue("whatsapp/general/whatsapp_number");
+    }
+
+    function getMessage()
+    {
+        return $this->scopeConfig->getValue("whatsapp/general/message");
+    }
+
+    function getPosition()
+    {
+        return $this->scopeConfig->getValue("whatsapp/general/position");
+    }
+
+    function getSize()
+    {
+        return $this->scopeConfig->getValue("whatsapp/general/size");
+    }
+}
